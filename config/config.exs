950c60bc58c3ce8acc5bb0d6a0c1dd912cdc94fb
Kamaro Lambert <kamaroly@gmail.com>
2023-10-30 15:01:53 +0300

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ config :entity, Entity.Repo,
   database: System.get_env("DATABASE_NAME", "entity"),
   username: System.get_env("DATABASE_USERNAME", "root"),
   password: System.get_env("DATABASE_PASSWORD", "root"),
-  hostname: System.get_env("DATABASE_HOST_NAME", "localhost"),
+  hostname: System.get_env("DATABASE_HOST_NAME", "127.0.0.1"),
   port: String.to_integer(System.get_env("DATABASE_PORT", "3306"))
 
 config :entity,
