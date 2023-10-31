713f40c6d3b3568d89d3e54e69d9ed38f793733a
Kamaro Lambert <kamaroly@gmail.com>
2023-10-31 19:57:33 +0300

    Update helpers.ex

--- lib/entity/helpers.ex
+++ lib/entity/helpers.ex
@@ -6,11 +6,7 @@ defmodule Entity.Helpers do
     iex> Entity.AppHelpers.app_name
     iex> :entity
   """
-  def app_name do
-    {app_name, _, _} = Enum.at(Application.loaded_applications(), 0)
-
-    app_name
-  end
+  def app_name, do: :entity
 
   def get_repo do
     Application.get_env(app_name(), :ecto_repos, [])
