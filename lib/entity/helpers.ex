60be4dc9b735c7c97d2637281076439f28a13068
Kamaro Lambert <kamaroly@gmail.com>
2023-10-31 19:41:39 +0300

    Update helpers.ex

--- lib/entity/helpers.ex
+++ lib/entity/helpers.ex
@@ -1,6 +1,19 @@
 defmodule Entity.Helpers do
+
+  @doc """
+  Returns the first loaded app name
+  ## Examples
+    iex> Entity.AppHelpers.app_name
+    iex> :entity
+  """
+  def app_name do
+    {app_name, _, _} = Enum.at(Application.loaded_applications(), 0)
+
+    app_name
+  end
+
   def get_repo do
-    Application.get_env(:entity, :ecto_repos, [])
+    Application.get_env(app_name(), :ecto_repos, [])
     |> case do
       [repo] -> repo
       _ -> raise "Multiple repos are not supported right now."
